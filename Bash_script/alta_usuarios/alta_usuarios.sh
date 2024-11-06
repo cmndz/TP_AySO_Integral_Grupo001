885d9afd34119a8e717000a8709d4442db697234
gsancineto <jgabrielsancineto@gmail.com>
2024-11-06 23:06:41 +0000

    add: se agrego logica a check_url.sh y se modifico alta_usuarios con mas checks

--- Bash_script/alta_usuarios/alta_usuarios.sh
+++ Bash_script/alta_usuarios/alta_usuarios.sh
@@ -14,7 +14,39 @@ clear
 ###############################
 
 LISTA=$1
+USUARIO_PARAMETRO=$2
+#Chequeo que el archivo haya sido proporcionado
+if [ -z "$LISTA" ]; then
+	echo "Archivo parametro no proporcionado"
+	exit 1
+fi
+
+#Chequeo que el archivo exista
+if [ ! -f "$LISTA" ]; then
+	echo "El archivo $LISTA no existe"
+	exit 1
+fi
+
+#Chequeo que el user haya sido proporcionado
+if [ -z "$USUARIO_PARAMETRO" ]; then
+	echo "Usuario parametro no proporcionado"
+	exit 1
+fi
+
+#Chequeo que el user exista
+if [ ! id $USUARIO_PARAMETRO %>/dev/null ]; then
+	echo "El usuario parametro no existe"
+	exit 1
+fi
+
 CLAVE=$(sudo cat /etc/shadow | grep $2 | awk -F ':' '{print $2}')
+
+#Chequeo si la clave es valida
+if [ -z "$CLAVE" ]; then
+	echo "No se pudo obtener la clave de $USUARIO_PARAMETRO"
+	exit 1
+fi
+
 
 ANT_IFS=$IFS
 
@@ -23,7 +55,14 @@ for LINEA in `cat $LISTA |  grep -v ^#`
 do
 	USUARIO=$(echo  $LINEA |awk -F ':' '{print $1}')
 	GRUPO=$(echo  $LINEA |awk -F ':' '{print $2}')
-	echo "sudo useradd -m -s /bin/bash -p $CLAVE -g $GRUPO $USUARIO"
+
+	#Chequeo que grupo no exista
+	if [!getent group "$GRUPO" &>/dev/null]; then
+		sudo groupadd "$GRUPO"
+	fi
+
+	#Creo el usuario
+	sudo useradd -m -s /bin/bash -p $CLAVE -g $GRUPO $USUARIO
 done
 IFS=$ANT_IFS
 
--- Bash_script/check_url/check_URL.sh
+++ Bash_script/check_url/check_URL.sh
@@ -15,22 +15,39 @@ LISTA=$1
 
 LOG_FILE="/var/log/status_url.log"
 
+#Chequeo que el parametro haya sido proporcionado
+if [ -z "$LISTA" ]; then
+	echo "Archivo de parametro no proporcionado"
+	exit 1
+fi
+
+#Chequeo que el archivo existe
+if [ ! -f "$LISTA" ]; then
+	echo "El archivo parametro no existe"
+	exit 1
+fi
+
 ANT_IFS=$IFS
 IFS=$'\n'
 
 #---- Dentro del bucle ----#
+for URL in $(cat "$LISTA"); do
   # Obtener el código de estado HTTP
   STATUS_CODE=$(curl -LI -o /dev/null -w '%{http_code}\n' -s "$URL")
 
   # Fecha y hora actual en formato yyyymmdd_hhmmss
   TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
+  #Creo el path del archivo log y nombre de arhivo
+  DIR=""
+  NOMBRE_ARCHIVO=""
 
  # Registrar en el archivo /var/log/status_url.log
   echo "$TIMESTAMP - Code:$STATUS_CODE - URL:$URL" |sudo tee -a  "$LOG_FILE"
 
+  echo "$TIMESTAMP - Code:$STATUS_CODE - URL:$URL" > "$DIR/$NOMBRE_ARCHIVO.log"
 
-
+done
 #-------------------------#
 
 IFS=$ANT_IFS
